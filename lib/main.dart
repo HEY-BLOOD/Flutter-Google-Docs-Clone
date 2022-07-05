6a1f21e6d4653c34698105052e7e83497f76e825
blood <hey-blood@foxmail.com>
2022-07-06 01:03:35 +0800

    Base Setup

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_docs_clone/app/app.dart';
+import 'package:google_docs_clone/app/utils.dart';
+
+void main() {
+  setupLogger();
+
+  runApp(const GoogleDocsApp());
+}
