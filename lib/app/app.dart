1716daf71314fac8f49e7621731c1387f24f190a
blood <hey-blood@foxmail.com>
2022-07-06 01:40:16 +0800

    Routemaster Setup

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/app/navigation/routes.dart';
+import 'package:routemaster/routemaster.dart';
 
 class GoogleDocsApp extends ConsumerStatefulWidget {
   const GoogleDocsApp({Key? key}) : super(key: key);
@@ -11,12 +13,11 @@ class GoogleDocsApp extends ConsumerStatefulWidget {
 class _GoogleDocsAppState extends ConsumerState<GoogleDocsApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('home'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+        return routesLoggedOut;
+      }),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
